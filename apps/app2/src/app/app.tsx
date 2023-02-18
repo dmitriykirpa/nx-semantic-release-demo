cf5af6f446926b755f712e9648b7b1e047572d3d
Dmytro Kirpa <dima.kirpa@people.ai>
2023-02-18 14:38:46 +0100

    refactor(app2): cleanup empty tags

--- apps/app2/src/app/app.tsx
+++ apps/app2/src/app/app.tsx
@@ -1,13 +1,7 @@
 import NxWelcome from './nx-welcome';
 
 export function App() {
-  return (
-    <>
-      <NxWelcome title="app2" />
-
-      <div />
-    </>
-  );
+  return <NxWelcome title="app2" />;
 }
 
 export default App;
